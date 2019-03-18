c238ec3dbc610256f6ea9c7187c93a06afeb6f59
Mohammed Sadiq <sidsmith9@gmail.com>
2019-03-19 00:14:48 +0530

    Fixed a length glitch

--- routes/proprietary.js
+++ routes/proprietary.js
@@ -1,35 +1,31 @@
-const express=require('express');
-const router=express.Router();
-const controller=require('../controller/cruds');
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/cruds');
 
-router.post('/', async (req,res) => {
-    const proprietary={
-        name : req.body.name,
-        shortDescription : req.body.shortDescription,
-        tags : req.body.tags
+router.post('/', async (req, res) => {
+    const proprietary = {
+        name: req.body.name,
+        shortDescription: req.body.shortDescription,
+        tags: req.body.tags
     };
-    const result=await controller.addProprietarySoftware(proprietary);
+    const result = await controller.addProprietarySoftware(proprietary);
     console.log(result);
     res.send(proprietary);
 });
 
-router.get('/', async (req,res)=>{
-    const proprietary=await controller.getAllProprietarySoftwares();
-    if(!proprietary){
-        res.status(404).send('No result found for proprietary softwares');
-        return;
-    }
+router.get('/', async (req, res) => {
+    const proprietary = await controller.getAllProprietarySoftwares();
+    if (proprietary.length == 0)
+        return res.status(404).send('No result found for proprietary softwares');
     console.log(proprietary);
     res.send(proprietary);
 });
 
-router.get('/:pattern', async (req,res)=>{
-    const proprietary=await controller.searchProprietarySoftwares(req.params.pattern);
-    if(!proprietary){
-        res.status(404).send('No search result found for'+req.params.pattern);
-        return;
-    }
+router.get('/:pattern', async (req, res) => {
+    const proprietary = await controller.searchProprietarySoftwares(req.params.pattern);
+    if (proprietary.length == 0)
+        return res.status(404).send('No search result found for' + req.params.pattern);
     res.send(proprietary);
 });
 
-module.exports=router;
+module.exports = router;
