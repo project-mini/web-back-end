b0fd6212ded17dfdd6068959c0a4187ae7d18b2c
m-zaink <sidsmith9@gmail.com>
2019-03-25 22:53:50 +0530

    Update usercredential.js with better RegExps

--- models/usercredentials.js
+++ models/usercredentials.js
@@ -1,49 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //schema for user credentials
 const user = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    gender: {
-        type: String,
-        lowercase: true,
-        enum: ['male', 'female', 'others']
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        // TODO : We can't be storing passwords as open string.
-        // Probably should implement a hash function.
-        type: String,
-        required: true
-    },
-    signUpTime: {
-        type: Date,
-        default: Date.now
-    },
-    email: {
-        type: String,
-        lowercase: true,
-        match: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/,
-        unique: true,
-        required: true
-    },
-    hasUpvoted: {
-        type: Array,
-        default: []
-    },
-    hasDownvoted: {
-        type: Array,
-        default: []
-    }
+  firstName: {
+    type: String,
+    required: true,
+    match: new RegExp("^[a-zA-Z ]+$")
+  },
+  lastName: {
+    type: String,
+    required: true,
+    match: new RegExp("^[a-zA-Z ]+$")
+  },
+  gender: {
+    type: String,
+    lowercase: true,
+    enum: ["male", "female", "others"]
+  },
+  username: {
+    type: String,
+    required: true,
+    match: new RegExp("^[a-zA-Z-_0-9]{3,20}$")
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  signUpTime: {
+    type: Date,
+    default: Date.now
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    match: new RegExp("^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$"),
+    unique: true,
+    required: true
+  },
+  hasUpvoted: {
+    type: Array,
+    default: []
+  },
+  hasDownvoted: {
+    type: Array,
+    default: []
+  }
 });
 
-module.exports = mongoose.model('UserCredentials', user);
+module.exports = mongoose.model("UserCredentials", user);
