da56e7e0a1c2ad03a314b0a8f808d10991cd26d5
m-zaink <sidsmith9@gmail.com>
2019-03-28 20:53:46 +0530

    Add RegExp for f- & -l names and enum for gender

--- models/usercredentials.js
+++ models/usercredentials.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
-const Joi = require('joi');
-const passwordComplexity = require('joi-password-complexity')(Joi);
+const Joi = require("joi");
+const PasswordComplexity = require("joi-password-complexity");
 
 //schema for user credentials
 const user = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
+    match: new RegExp("^[a-zA-Z' ]{1,50}$")
   },
   lastName: {
     type: String,
     required: true,
+    match: new RegExp("^[a-zA-Z' ]{1,50}$")
   },
   gender: {
     type: String,
@@ -20,7 +22,8 @@ const user = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    match: new RegExp("^[a-zA-Z\-_0-9]{3,30}$")
+    match: new RegExp("^[a-zA-Z0-9]{3,30}$"),
+    unique: true
   },
   password: {
     type: String,
@@ -29,7 +32,7 @@ const user = new mongoose.Schema({
   email: {
     type: String,
     lowercase: true,
-    match: new RegExp("^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$"),
+    match: new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"),
     maxlength: 255,
     unique: true,
     required: true
@@ -46,15 +49,31 @@ const user = new mongoose.Schema({
 
 function validateUser(user) {
   const schema = {
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
-    gender: Joi.string().required().options(['male', 'female', 'something-else']),
-    username: Joi.string().required().min(3).max(30).regex(new RegExp("^[a-zA-Z\-_0-9]{3,30}$")),
-    email: Joi.required().email(),
-    password: Joi.passwordComplexity()
-  }  
+    firstName: Joi.string()
+      .required()
+      .regex(new RegExp("^[a-zA-Z' ]{1,50}$")),
+    lastName: Joi.string()
+      .required()
+      .regex(new RegExp("^[a-zA-Z' ]{1,50}$")),
+    gender: Joi.string()
+      .required()
+      .valid("male", "female", "something-else"),
+    username: Joi.string()
+      .required()
+      .min(3)
+      .max(30)
+      .regex(new RegExp("^[a-zA-Z-_0-9]{3,30}$")),
+    email: Joi.string()
+      .required()
+      .email(),
+    password: Joi.string().required()
+  };
 
-  return Joi.validate(user, schema);
+  const schemaError = Joi.validate(user, schema);
+  const passwordError = Joi.validate(user.password, new PasswordComplexity());
+
+  if (schemaError) return schemaError;
+  if (passwordError) return passwordError;
 }
 
 exports.User = mongoose.model("UserCredentials", user);
