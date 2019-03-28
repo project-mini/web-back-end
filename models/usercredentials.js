35ed72c61ed7f3f1ae037ba9ca815810fa8e9468
m-zaink <sidsmith9@gmail.com>
2019-03-28 05:45:05 +0530

    Update usercredentials.js

--- models/usercredentials.js
+++ models/usercredentials.js
@@ -5,35 +5,30 @@ const user = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
-    match: new RegExp("^[a-zA-Z ]+$")
   },
   lastName: {
     type: String,
     required: true,
-    match: new RegExp("^[a-zA-Z ]+$")
   },
   gender: {
     type: String,
     lowercase: true,
-    enum: ["male", "female", "others"]
+    enum: ["male", "female", "something-else"]
   },
   username: {
     type: String,
     required: true,
-    match: new RegExp("^[a-zA-Z-_0-9]{3,20}$")
+    match: new RegExp("^[a-zA-Z\-_0-9]{3,20}$")
   },
   password: {
     type: String,
     required: true
   },
-  signUpTime: {
-    type: Date,
-    default: Date.now
-  },
   email: {
     type: String,
     lowercase: true,
     match: new RegExp("^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$"),
+    maxlength: 255,
     unique: true,
     required: true
   },
