836985160768b164960ca23df0b86038355dc923
m-zaink <sidsmith9@gmail.com>
2019-04-01 20:49:52 +0530

    Update sign_up.js to handler sign-up better
    Middlewares were added such that:
    1. User shall be registered.
    2. User shall be logged in right away.

--- routes/sign_up.js
+++ routes/sign_up.js
@@ -1,39 +1,14 @@
-const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
 
-const { User, validateUser } = require("../models/usercredentials");
-const crudsSignUp = require("../controller/cruds_sign_up");
+const authenticate = require("../middleware/authenticate");
+const register = require("../middleware/register");
 
-router.post("/", async (req, res) => {
-  const { error } = validateUser(req.body);
-  if (error) return res.status(400).send(error["message"]);
 
-  let user = await User.findOne({ email: req.body.email });
-
-  if (user) return res.status(400).send("User already registered!");
-
-  user = new User(
-    _.pick(req.body, [
-      "firstName",
-      "lastName",
-      "email",
-      "password",
-      "gender",
-      "username"
-    ])
-  );
-  try {
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
-    
-    const result = await crudsSignUp.addUser(user);
-    res.send(_.pick(result, ["firstName", "lastName", "username"]));
-  } catch (err) {
-    console.log(err);
-    res.status(400).send(err["message"]);
-  }
+router.post("/", [register, authenticate], async (req, res) => {
+  res
+  .header("x-auth-token", req.body.user.generateAuthToken())
+  .send(`Welcome ${req.body.user.username}`);
 });
 
 module.exports = router;
