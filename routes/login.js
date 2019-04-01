971a9c4db0df9c85a1a84ef904e531cfc1a36963
m-zaink <sidsmith9@gmail.com>
2019-04-01 07:59:26 +0530

    Use config variables for re-route url

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const config = require('config');
 const github = require("debug")("github");
 const router = express.Router();
 const session = require("express-session");
@@ -7,7 +8,7 @@ const qs = require("querystring");
 const _ = require("lodash");
 const randomString = require("randomstring");
 // const redirect_uri = "https://dry-dusk-50998.herokuapp.com/api/redirect";
-const redirect_uri = "http://localhost:3000/api/redirect";
+// const redirect_uri = "http://localhost:3000/api/redirect";
 
 router.get("/", (req, res) => {
   req.session.csrf_string = randomString.generate();
@@ -18,8 +19,8 @@ router.get("/", (req, res) => {
   const githubAuthUrl =
     "https://github.com/login/oauth/authorize?" +
     qs.stringify({
-      client_id: process.env.ALT_CLIENT_ID,
-      redirect_uri: req.body.redirect_uri,
+      client_id: config.get('client_id'),
+      redirect_uri: config.get('redirect_uri'),
       state: req.session.csrf_string,
       scope: "user:email"
     });
