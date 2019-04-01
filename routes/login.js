c0ee3df3c1ba22441fd9e7681854a26ac2081ea3
m-zaink <sidsmith9@gmail.com>
2019-04-01 07:25:09 +0530

    Minor changes

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const github = require("debug")("github");
 const router = express.Router();
 const session = require("express-session");
 const request = require("request");
@@ -8,17 +9,17 @@ const randomString = require("randomstring");
 // const redirect_uri = "https://dry-dusk-50998.herokuapp.com/api/redirect";
 const redirect_uri = "http://localhost:3000/api/redirect";
 
-
-
 router.get("/", (req, res) => {
   req.session.csrf_string = randomString.generate();
-  console.log(req.session.csrf_string);
-  console.log(process.env.ALT_CLIENT_ID);
+
+  github(req.session.csrf_string);
+  github(process.env.ALT_CLIENT_ID);
+
   const githubAuthUrl =
     "https://github.com/login/oauth/authorize?" +
     qs.stringify({
       client_id: process.env.ALT_CLIENT_ID,
-      redirect_uri: redirect_uri,
+      redirect_uri: req.body.redirect_uri,
       state: req.session.csrf_string,
       scope: "user:email"
     });
