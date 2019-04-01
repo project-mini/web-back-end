2fad104ce6598ccf544eabf6a83ee35b353c7a93
m-zaink <sidsmith9@gmail.com>
2019-04-01 20:56:04 +0530

    Minor changes to login.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,20 +1,45 @@
-const bcrypt = require('bcrypt');
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models/usercredentials");
 
-module.exports = async function (req, res, next) {
-    try {
-        const err = Error("Incorrect email or password!");
-
-        const user = await User.findOne({ email: req.body.email });
-        if (!user) throw err;
-    
-        const result = await bcrypt.compare(req.body.password, user.password);
-        if (!result) throw err;
-        
-        req.user = user;
-        next();
-    } catch (err) {
-        return res.status(400).send(err.message);
+module.exports = async function(req, res, next) {
+  try {
+    const err = Error("Incorrect email or password!");
+    const token = req.header("x-auth-token");
+    let result;
+    let user;
+    if (token) {
+      try {
+        const decoded = jwt.verify(token, process.env.ALT_JWT_PRIVATE_KEY);
+        user = await User.findOne({
+          username: decoded.username
+        });
+      } catch (err) {
+        return res.status(400).send("Inavalid token");
+      }
+      if (!user) throw err;
+    } else {
+      if (!req.body.username_email) {
+        user = await User.findOne({ username: req.body.user.username });
+      } else if (req.body.username_email.length > 50) {
+        throw err;
+      } else {
+        user = await User.findOne({
+          $or: [
+            { email: req.body.username_email },
+            { username: req.body.username_email }
+          ]
+        });
+      }
+      if (!user) throw err;
+      result = await bcrypt.compare(req.body.password, user.password);
+      if (!result) throw err;
     }
-}
+
+    req.body.user = user;
+
+    next();
+  } catch (err) {
+    return res.status(400).send(err.message);
+  }
+};
--- routes/login.js
+++ routes/login.js
@@ -1,33 +1,14 @@
 const express = require("express");
-const config = require('config');
-const github = require("debug")("github");
 const router = express.Router();
-const session = require("express-session");
-const request = require("request");
-const qs = require("querystring");
-const _ = require("lodash");
-const randomString = require("randomstring");
-// const redirect_uri = "https://dry-dusk-50998.herokuapp.com/api/redirect";
-// const redirect_uri = "http://localhost:3000/api/redirect";
 
-router.get("/", (req, res) => {
-  req.session.csrf_string = randomString.generate();
+router.post("/", authenticate, (req, res) => {
+  res
+    .header("x-auth-token", req.body.user.generateAuthToken())
+    .send(`Welcome ${req.body.user.username}`);
+});
 
-  github(req.session.csrf_string);
-  github(process.env.ALT_CLIENT_ID);
-  console.log(config.get('client_id'));
-  console.log(config.get('redirect_uri'));
-  const githubAuthUrl =
-    "https://github.com/login/oauth/authorize?" +
-    qs.stringify({
-      client_id: process.env.ALT_CLIENT_ID,
-      redirect_uri: process.env.ALT_REDIRECT,
-      state: req.session.csrf_string,
-      scope: "user:email"
-    });
-
-  // Redirect to githubAuthURL for github to handle the authentication now.
-  res.redirect(githubAuthUrl);
+router.post("/me", authenticate, (req, res) => {
+  res.send(_.pick(req.body.user, ["username", "firstName", "lastName", "email"]));
 });
 
 module.exports = router;
