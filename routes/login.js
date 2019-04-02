90ac961ff239f9b537eeb47dc6322e2352af2afb
m-zaink <sidsmith9@gmail.com>
2019-04-02 08:01:41 +0530

    Minor changed to login.js

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const authenticate = require("../middleware/authenticate");
 router.post("/", authenticate, (req, res) => {
+  if(req.header('x-auth-token'))
+    return res.send(`Welcome ${req.body.user.username}`);
+
   res
     .header("x-auth-token", req.body.user.generateAuthToken())
     .send(`Welcome ${req.body.user.username}`);
